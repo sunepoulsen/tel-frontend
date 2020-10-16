0335734c350ffd261299d9efb558954568b2c2dd
sunepoulsen <sunepoulsen@gmail.com>
2020-10-16 22:32:35 +0200

    TEL-40: Reorganized unit test files and add unit tests of the HomeContent component.

--- tests/unit/app/modules/home/components/HomeContent.spec.ts
+++ tests/unit/app/modules/home/components/HomeContent.spec.ts
@@ -2,8 +2,8 @@ import { expect } from 'chai'
 import { shallowMount } from '@vue/test-utils'
 import HomeContent from '@/app/modules/home/components/HomeContent.vue'
 
-describe('HomeContent.vue', () => {
-  it('renders props.msg when passed', () => {
+describe('HomeContent', () => {
+  it('renders properties when passed', () => {
     const msg = 'new message'
     const wrapper = shallowMount(HomeContent, {
       propsData: { msg }
