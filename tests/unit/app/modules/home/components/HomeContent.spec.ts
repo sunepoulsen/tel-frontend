8ebffa8a36eb26af3cdaedead240f3ef66f0685e
sunepoulsen <sunepoulsen@gmail.com>
2020-10-18 22:17:28 +0200

    TEL-40: Add support of jest in unit tests.

--- tests/unit/app/modules/home/components/HomeContent.spec.ts
+++ tests/unit/app/modules/home/components/HomeContent.spec.ts
@@ -2,12 +2,13 @@ import { expect } from 'chai'
 import { shallowMount } from '@vue/test-utils'
 import HomeContent from '@/app/modules/home/components/HomeContent.vue'
 
-describe('HomeContent', () => {
-  it('renders properties when passed', () => {
+describe("HomeContent", () => {
+  test('Render msg prop', () => {
     const msg = 'new message'
     const wrapper = shallowMount(HomeContent, {
       propsData: { msg }
     })
+
     expect(wrapper.text()).to.include(msg)
   })
 })
